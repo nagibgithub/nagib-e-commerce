c78e2d08ac09ebae4250d2b8317843c919d7f345
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-04-20 13:18:45 +0600

    login and signup framework complete

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import {faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 const Login = () => {
+
+    const [eye, setEye] = useState(false)
+
     return (
-        <div className='my-10 mx-auto w-1/2 border-2 p-5 rounded-lg '>
-            <h1>this is login</h1>
-            <h1>New to Nagib E-Commerce?<Link className='text-[#ff9900]' to={'/signup'}> Create New Account</Link> first</h1>
+        <div className='form-field'>
+            <div className='m-[40px]'>
+                <h1 className='login-title'>Login</h1>
+                <form action="">
+                    <div className='flex flex-col'>
+                        <label className='label-field' htmlFor="" for='email'>Email</label>
+                        <input className='input-field' name='email' placeholder='Enter your @ Email' id='email' type="email" required />
+                    </div>
+                    <div className='flex flex-col'>
+                        <label className='label-field' htmlFor="" for='password'>Password</label>
+                        <input className='input-field' name='password' placeholder='Enter your password' id='password' type={eye?"password":"text"} required />
+                        <div className='flex items-center cursor-pointer' onClick={() => setEye(!eye)} ><span className='pr-2 py-2'>{eye?'See your password':'Hide your password'} </span> <FontAwesomeIcon className='text-sm' icon={eye?faEye:faEyeSlash} /></div>
+                    </div>
+                    <input className='submit-button' type="submit" value={'Login'}/>
+                </form>
+                <h1 className='text-center text-sm mb-5'>New to Nagib E-Commerce?<Link className='text-[#ff9900]' to={'/signup'}> Create New Account</Link> first</h1>
+                <div className='grid grid-cols-7 items-center mb-5'>
+                    <hr className='col-span-3 border border-[#95A0A7]'/>
+                    <h2 className='col-span-1 text-center text-[#323536]'>or</h2>
+                    <hr className='col-span-3 border border-[#95A0A7]'/>
+                </div>
+                <button className='w-full flex items-center justify-center border border-[#95a0a7] h-14 rounded text-lg'><img className='w-8 mr-2' src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="" />Continue with Google</button>
+            </div>
         </div>
     );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import {faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 const SignUp = () => {
+    const [eye, setEye] = useState(false)
     return (
-        <div>
-            <h1>Sign Up</h1>
-            <form action="">
-                <div>
-                    <label htmlFor="email">Enter Your Email</label>
-                    <input name='email' id="email" type="email" />
+        <div className='form-field'>
+            <div className='m-[40px]'>
+                <h1 className='login-title'>Sign Up</h1>
+                <form action="">
+                    <div className='flex flex-col'>
+                        <label className='label-field' htmlFor="" for='email'>Email</label>
+                        <input className='input-field' name='email' placeholder='Enter your @ Email' id='email' type="email" required />
+                    </div>
+                    <div className='flex flex-col'>
+                        <label className='label-field' htmlFor="" for='password'>Password</label>
+                        <input className='input-field' name='password' placeholder='Enter your password' id='password' type={eye ? "password" : "text"} required />
+                    </div>
+                    <div className='flex flex-col'>
+                        <label className='label-field' htmlFor="" for='confirm-password'>Confirm Password</label>
+                        <input className='input-field' name='confirm-password' placeholder='Re-Type your password' id='confirm-password' type={eye ? "password" : "text"} required />
+                        <div className='flex items-center cursor-pointer' onClick={() => setEye(!eye)} ><span className='pr-2 py-2'>{eye ? 'See your password' : 'Hide your password'} </span> <FontAwesomeIcon className='text-sm' icon={eye ? faEye : faEyeSlash} /></div>
+                    </div>
+                    <input className='submit-button' type="submit" value={'Sign Up'} />
+                </form>
+                <h1 className='text-center text-sm mb-5'>Already have an Account<Link className='text-[#ff9900]' to={'/login'}> log in</Link> Please</h1>
+                <div className='grid grid-cols-7 items-center mb-5'>
+                    <hr className='col-span-3 border border-[#95A0A7]' />
+                    <h2 className='col-span-1 text-center text-[#323536]'>or</h2>
+                    <hr className='col-span-3 border border-[#95A0A7]' />
                 </div>
-            </form>
-            <h1>If You have already an Id you can <Link className='text-[#ff9900]' to={'/login'}>Log in</Link></h1>
+                <button className='w-full flex items-center justify-center border border-[#95a0a7] h-14 rounded text-lg'><img className='w-8 mr-2' src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="" />Continue with Google</button>
+            </div>
         </div>
     );
 };
